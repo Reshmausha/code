e603b5ae589ace9189e9da82f8f62bf8aa90f938
Reshmausha <41889853+Reshmausha@users.noreply.github.com>
2018-07-30 12:11:07 +0530

    Update positive.C

--- positive.C
+++ positive.C
@@ -1,8 +1,24 @@
-void main()
+#include <stdio.h>
+int main()
 {
-int a;
-scanf("%d",&a):
-if(a<0)
-{
-printf("negative");}
+    int number, originalNumber, remainder, result = 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+    return 0;
 }
