abfe400138d06d37f85edbee4884a2c15951ced2
Reshmausha <41889853+Reshmausha@users.noreply.github.com>
2018-08-06 12:07:04 +0530

    Create pos or neg.c

---
+++ pos or neg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the value");
+scanf("%d",&n);
+if(n>0)
+{
+printf("n is positive",&n);
+}
+elseif(n<0)
+{
+printf("n ie negative",&n);
+}
+else(n==0)
+{
+printf("n is 0");
+}
